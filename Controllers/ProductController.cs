89f5a5721b92c28fd0ad928f5f2f07767fd22783
DuyNguyenlk22 <nguyenduylk202@gmail.com>
2024-04-21 22:23:27 +0700

    update ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<ProductModel> products = [
+                new () {Id = 1 , Name = "Product 1" , Available = true , Price = 100000 , PromotionPrice = 20000},
+                new () {Id = 2 , Name = "Product 2" , Available = false , Price = 200000 , PromotionPrice = 2000}
+        ];
+        public IActionResult Index()
+        {
+            return Json(products);
+            //return View(products);
+        }
+    }
+}
